32f2b16f4e50f087d91f25710a76db2e4c729eb9
aeinstein <mb@tellimate.de>
2025-02-01 13:21:38 +0100

    blah

--- rc_server/scripts/stream.sh
+++ rc_server/scripts/stream.sh
@@ -1,12 +1,20 @@
 #!/bin/bash
 
+
+# stream libcam h264 -> ffmpeg copy -> rtmp server
 #libcamera-vid -t 0 -g 10 --codec h264 --inline -n --flush -o - \
 #ffmpeg -i - -vcodec copy -f flv rtmp://127.0.0.1:1935/demo/live
 
 
+# Streaming libcam raw yuv420 -> ffmpeg h264 -> rtmp server
 libcamera-vid -t 0 -g 10 --codec yuv420 --inline -n --flush -o - | \
-ffmpeg -f rawvideo -vcodec rawvideo -s 640x480 -r 30 -pix_fmt yuv420p -thread_queue_size 128 -i - -f pulse -i default \
+ffmpeg -f rawvideo -vcodec rawvideo -s 640x480 -r 30 -pix_fmt yuv420p -rtbufsize 1M -i - -f pulse -i default \
 -vf drawtext="fontfile=monofonto.ttf: fontsize=20: box=1: boxcolor=black@0.75: boxborderw=5: fontcolor=white: x=(w-text_w)/2: y=((h-text_h)/2)+((h-text_h)/4): text='%{gmtime\:%M\\\\\:%S}'" \
--vcodec libx264 -b:v 2M -s 640x480 -preset ultrafast -profile:v main -x264-params keyint=30:scenecut=0 -acodec aac -ac 1 -ar 44100  -f flv rtmp://127.0.0.1/demo/live
+-vcodec libx264 -b:v 2M -s 640x480 -preset ultrafast -profile:v main -x264-params keyint=30:scenecut=0 -acodec aac -ac 1 -ar 44100 -shortest -f flv rtmp://127.0.0.1/demo/live
+
+
+# stream libcam libav -> rtmp server
 
 #-x264-params keyint=120:min-keyint=30:scenecut=0 -movflags +faststart
+#libcamera-vid -t 0 -g 10 --codec libav --libav-video-codec libx264 --inline -n --flush -o rtmp://127.0.0.1:1935/demo/live
+
